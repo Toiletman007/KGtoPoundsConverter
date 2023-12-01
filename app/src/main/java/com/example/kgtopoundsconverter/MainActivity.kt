5047263385981cebc2774408c2612f41ee002bc9
Wc man <Quertical5@gmail.com>
2023-12-01 15:19:27 +0100

    Declared all Widgets.

--- app/src/main/java/com/example/kgtopoundsconverter/MainActivity.kt
+++ app/src/main/java/com/example/kgtopoundsconverter/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.kgtopoundsconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ttl: TextView = findViewById(R.id.title)
+        val userInputKG: EditText = findViewById(R.id.kiloEditText)
+        val btn: Button = findViewById(R.id.btn)
+        val userOutputLBs: TextView = findViewById(R.id.poundsTextView)
+
+
+
 
 
     }
